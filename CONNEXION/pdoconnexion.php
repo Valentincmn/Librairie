005242223edaf06894a594b5e81a742c1105fe2c
Valentin Cmn <valentincmn@MacBook-Pro-de-Valentin.local>
2025-03-18 09:08:04 +0100

    Correction de l'affichage des messages d'erreur dans le formulaire de connexion

--- CONNEXION/pdoconnexion.php
+++ CONNEXION/pdoconnexion.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start(); 
 
 $servername = "localhost";
@@ -10,39 +9,39 @@ if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $mdp = $_POST['password'];
 
-    
     if (empty($email) || empty($mdp)) {
-        echo "Veuillez remplir tous les champs.";
+        $_SESSION['error'] = "Veuillez remplir tous les champs.";
+        header("Location: login.php");
+        exit();
     } else {
         try {
-            
             $bdd = new PDO("mysql:host=$servername;dbname=membre", $username, $password);
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            
             $sql = 'SELECT * FROM users WHERE email = ?';
             $result = $bdd->prepare($sql);
             $result->execute([$email]);
-
-            
             $data = $result->fetch(PDO::FETCH_ASSOC);
 
-            
             if (!$data) {
-                echo "Email incorrect ou non trouvé.";
+                $_SESSION['error'] = "Email incorrect ou non trouvé.";
+                header("Location: login.php");
+                exit();
             } elseif (isset($data["mdp"]) && password_verify($mdp, $data["mdp"])) {
                 $_SESSION['email'] = $data['email'];
                 $_SESSION['id'] = $data['id'];
-                
                 header("Location: ../ACCUEIL/header 2.php");
                 exit();
             } else {
-                echo "Mot de passe incorrect.";
+                $_SESSION['error'] = "Mot de passe ou Email incorrect.";
+                header("Location: login.php");
+                exit();
             }
         } catch (PDOException $e) {
-            echo "Erreur : " . $e->getMessage();
+            $_SESSION['error'] = "Erreur de connexion à la base de données.";
+            header("Location: login.php");
+            exit();
         }
     }
-    
 }
 ?>
